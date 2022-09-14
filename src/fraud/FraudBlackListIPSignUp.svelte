6b23ad9c35a2a5e3d13287f95b13f5df29a0438e
Jason Young <jasonyeong790@gmail.com>
2022-09-14 11:30:39 +0900

    프로드 등록 팝업

--- src/fraud/FraudBlackListIPSignUp.svelte
+++ src/fraud/FraudBlackListIPSignUp.svelte
@@ -5,26 +5,14 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">파트너 등록</h5>
+          <h5 class="modal-title">IP 주소 등록</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-           <div class='label'>회사 혹은 플랫폼 이름</div> 
+           <div class='label'>IP 주소</div> 
            <div class='text'>
-               <input type='textbox'/>
-            </div>
-           <div class='label'>매니저 성함</div> 
-           <div class='text'>
-               <input type='textbox'/>
-            </div>
-            <div class='label'>매니저 이메일</div> 
-            <div class='text'>
-               <input type='textbox'/>
-            </div>
-            <div class='label'>매니저 연락처</div> 
-            <div class='text'>
                <input type='textbox'/>
             </div>
             <div class='label'>설명</div> 
--- src/fraud/FraudBlackListPubSignUp.svelte
+++ src/fraud/FraudBlackListPubSignUp.svelte
@@ -5,29 +5,25 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">파트너 등록</h5>
+          <h5 class="modal-title">퍼블리셔 ID 등록</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-           <div class='label'>회사 혹은 플랫폼 이름</div> 
+           <div class='label'>Publisher(Site) ID</div> 
            <div class='text'>
                <input type='textbox'/>
             </div>
-           <div class='label'>매니저 성함</div> 
+           <div class='label'>Sub Publisher(Site) ID</div> 
            <div class='text'>
                <input type='textbox'/>
             </div>
-            <div class='label'>매니저 이메일</div> 
+            <div class='label'>광고 파트너</div> 
             <div class='text'>
                <input type='textbox'/>
             </div>
-            <div class='label'>매니저 연락처</div> 
-            <div class='text'>
-               <input type='textbox'/>
-            </div>
-            <div class='label'>설명</div> 
+            <div class='label'>사유</div> 
             <div class='text'>
                <textarea/>
             </div>
