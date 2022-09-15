551ff01c176410b98e195ebe61ba37896cc7fa5a
jasonyeong790 <jasonyeong790@pood.pet>
2022-09-15 18:14:50 +0900

    SDK 연동 가이드 이벤트 분석 페이지 추가

--- src/App.svelte
+++ src/App.svelte
@@ -8,8 +8,10 @@
 	import Fraud 				from "./Fraud.svelte"
 	import Tracking 			from "./Tracking.svelte"
 	import Attr 				from "./Attr.svelte"
-	import SDKGuide1 			from "./SDKGuide1.svelte";	
 	import SDKGuide2 			from "./SDKGuide2.svelte";	
+	import SDKGuide1P1 			from "./sdkguide1/page1.svelte";	
+	import SDKGuide1P2 			from "./sdkguide1/page2.svelte";	
+	import SDKGuide1P3 			from "./sdkguide1/page3.svelte";	
 </script>
 
 <main>
@@ -32,7 +34,12 @@
 					<a href ="/attr"><ul class='nav-link-li'><i class="bi bi-bar-chart nav-icon"></i>측정 모델 </ul></a>
 					<ul class='nav-link-li'>
 						<i class="bi bi-gear nav-icon"></i>SDK 연동 가이드 
-						<a href ="/sdkguide1"><li class='sdk'>iOS SDK</li></a>
+						<li class='sdk'>iOS SDK</li>
+						<ul class='sdk'>
+							<a href="/sdkguide1p1"><li>페트리 초기화</li></a>
+							<a href="/sdkguide1p2"><li>딥 링크 이벤트 분석</li></a>
+							<a href="/sdkguide1p3"><li>이벤트 분석</li></a>
+						</ul>
 						<a href ="/sdkguide2"><li class='sdk'>Android SDK</li></a>
 					</ul>
 				</li>
@@ -85,14 +92,26 @@
 					<Partner/>
 				</Route>
 				
-				<!-- Router : SDK Guide 1 -->
-				<Route path="/sdkguide1" primary={false}>
-					<SDKGuide1/>
-				</Route>
+ 
 
 				<!-- Router : SDK Guide 2 -->
 				<Route path="/sdkguide2" primary={false}>
 					<SDKGuide2/>
+				</Route>
+
+				<!-- Router : SDK Guide 1 - Page 1 -->
+				<Route path="/sdkguide1p1" primary={false}>
+					<SDKGuide1P1/>
+				</Route>
+
+				<!-- Router : SDK Guide 1 - Page 2 -->
+				<Route path="/sdkguide1p2" primary={false}>
+					<SDKGuide1P2/>
+				</Route>
+
+				<!-- Router : SDK Guide 1 - Page 3 -->
+				<Route path="/sdkguide1p3" primary={false}>
+					<SDKGuide1P3/>
 				</Route>
 
 
